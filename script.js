bfb45d25eb43d97aad9c2d023d9d05ff49798954
tozummi <tozummi@gmail.com>
2026-07-17 03:04:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,2 @@
-const latitude = 51.448009;
-const longitude = -2.027951;
-
-async function testPrayerTimes() {
-
-  const today = new Date();
-
-  const day = today.getDate();
-  const month = today.getMonth() + 1;
-  const year = today.getFullYear();
-
-  const url = `https://api.aladhan.com/v1/timings/${day}-${month}-${year}?latitude=${latitude}&longitude=${longitude}&method=3`;
-
-  try {
-    const response = await fetch(url);
-
-    const data = await response.json();
-
-    alert(JSON.stringify(data.data.timings));
-
-  } catch (error) {
-    alert("ERROR: " + error);
-  }
-}
-
-testPrayerTimes();
+console.log("NEW SCRIPT LOADED");
+alert("NEW SCRIPT LOADED");
