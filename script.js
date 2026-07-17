1ed56ca7b98004c1d8cc5a9b6980de2c38c8c195
tozummi <tozummi@gmail.com>
2026-07-17 02:35:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,90 @@
 const latitude = 51.448009;
 const longitude = -2.027951;
 
+
 const coordinates = new adhan.Coordinates(
-  latitude,
-  longitude
+latitude,
+longitude
 );
 
-const date = new Date();
 
 const params = adhan.CalculationMethod.MuslimWorldLeague();
 
-params.madhab = adhan.Madhab.Shafi;
+
+function updatePrayerTimes(){
+
+const now = new Date();
 
 const prayerTimes = new adhan.PrayerTimes(
-  coordinates,
-  date,
-  params
+coordinates,
+now,
+params
 );
 
 
-function formatTime(time) {
-  return time.toLocaleTimeString([], {
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: false
-  });
+function format(time){
+return time.toLocaleTimeString([],{
+hour:"2-digit",
+minute:"2-digit",
+hour12:false
+});
 }
 
 
-document.getElementById("fajr").textContent =
-  formatTime(prayerTimes.fajr);
+document.getElementById("fajr").innerText=format(prayerTimes.fajr);
+document.getElementById("dhuhr").innerText=format(prayerTimes.dhuhr);
+document.getElementById("asr").innerText=format(prayerTimes.asr);
+document.getElementById("maghrib").innerText=format(prayerTimes.maghrib);
+document.getElementById("isha").innerText=format(prayerTimes.isha);
 
-document.getElementById("sunrise").textContent =
-  formatTime(prayerTimes.sunrise);
 
-document.getElementById("dhuhr").textContent =
-  formatTime(prayerTimes.dhuhr);
 
-document.getElementById("asr").textContent =
-  formatTime(prayerTimes.asr);
+const prayers=[
+["Fajr",prayerTimes.fajr],
+["Dhuhr",prayerTimes.dhuhr],
+["Asr",prayerTimes.asr],
+["Maghrib",prayerTimes.maghrib],
+["Isha",prayerTimes.isha]
+];
 
-document.getElementById("maghrib").textContent =
-  formatTime(prayerTimes.maghrib);
 
-document.getElementById("isha").textContent =
-  formatTime(prayerTimes.isha);
+let next=null;
+
+
+for(let prayer of prayers){
+if(prayer[1]>now){
+next=prayer;
+break;
+}
+}
+
+
+if(!next){
+
+next=[
+"Fajr",
+new Date(prayerTimes.fajr.getTime()+86400000)
+];
+
+}
+
+
+const difference=next[1]-now;
+
+
+const hours=Math.floor(difference/3600000);
+
+const minutes=Math.floor(
+(difference%3600000)/60000
+);
+
+
+document.getElementById("nextPrayer").innerText =
+`${next[0]} in ${hours}h ${minutes}m`;
+
+}
+
+
+updatePrayerTimes();
+
+setInterval(updatePrayerTimes,60000);
